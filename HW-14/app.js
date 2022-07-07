74ce106d05b72410c07021c840d06e6080cbcb18
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2022-07-07 17:36:57 +0600

    add hw-14

---
+++ HW-14/app.js
@@ -0,0 +1,6 @@
+document.querySelector(".burger").addEventListener('click', function(){
+    document.querySelector('.burger','.header-nav').classList.toggle('active');
+    document.querySelector('.header-nav').classList.toggle('open');
+ });
+ 
+ 
